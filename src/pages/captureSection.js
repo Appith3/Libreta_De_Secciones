f4fb94a660dff57dd137c6c4ea89e68a3344ed8d
appithe <joelaviles413@gmail.com>
2023-12-14 16:43:47 -0600

    Finish Design

--- src/pages/captureSection.js
+++ src/pages/captureSection.js
@@ -1,13 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
 
 const CaptureSection = ({ navigation, route }) => {
 
-	const { cadenamiento } = route.params
+	const { cadenamiento } = route.params // pasado como parámetro dentro de route en el componente ListItem
+
+	const { codigo, nombre, lectura_central } = cadenamiento
+
+	const [_codigo, setCodigo] = useState(codigo)
+	const [_nombre, setNombre] = useState(nombre)
+	const [_lecturaCentral, setLecturaCentral] = useState(lectura_central)
 
 	useEffect(() => {
-		navigation.setOptions({ title: cadenamiento.nombre })
+		navigation.setOptions({ title: `${_nombre} CENTRO` || 'Nueva Sección centro' })
+		console.log("cadenamiento: ", cadenamiento);
+		console.log("route: ", route);
 	}, [])
 
 	const onPressLeft = () => {
@@ -18,34 +26,44 @@ const CaptureSection = ({ navigation, route }) => {
 		navigation.navigate("captureSectionSides", { _side: "right" })
 	}
 
+	// formateamos el valor del cadenamiento de 0000 a 0+000.00
+	const formatAlignmentName = () => {}
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.main} >
-				<TextInput
-					mode="outlined"
-					placeholder="Codigo"
-					// value={projectName}
-					// onChangeText={projectName => setProjectName(projectName)}
-					right={<TextInput.Icon icon="tag" />} />
+				<View style={styles.form} >
+					<TextInput
+						mode="outlined"
+						placeholder="Codigo"
+						value={_codigo}
+						onChangeText={_codigo => setCodigo(_codigo)}
+						right={<TextInput.Icon icon="tag" />} />
 
-				<TextInput
-					mode="outlined"
-					placeholder="Cadenamiento"
-					keyboardType="number-pad"
-					inputMode="decimal"
-					// value={projectName}
-					// onChangeText={projectName => setProjectName(projectName)}
-					right={<TextInput.Icon icon="map-marker" />} />
-
-				<TextInput
-					mode="outlined"
-					placeholder="Lectura central"
-					keyboardType="number-pad"
-					inputMode="decimal"
-					textAlign="left"
-					// value={projectName}
-					// onChangeText={projectName => setProjectName(projectName)}
+					<TextInput
+						mode="outlined"
+						placeholder="Cadenamiento"
+						keyboardType="number-pad"
+						inputMode="decimal"
+						value={_nombre}
+						onChangeText={_nombre => setNombre(_nombre)}
+						right={<TextInput.Icon icon="map-marker" />}
 					/>
+
+					<TextInput
+						mode="outlined"
+						placeholder="Lectura central"
+						keyboardType="number-pad"
+						inputMode="decimal"
+						textAlign="left"
+						value={_lecturaCentral}
+						onChangeText={_lecturaCentral => setLecturaCentral(_lecturaCentral)} />
+				</View>
+				<View style={styles.controls} >
+					<Button icon="chevron-left" onPress={onPressLeft} uppercase mode="contained" >Capturar izquierda</Button>
+					<Button uppercase mode="outlined" textColor="#F5F7FA" >Igual a la anterior</Button>
+					<Button icon="chevron-right" onPress={onPressRight} uppercase mode="contained" >Capturar derecha</Button>
+				</View>
 			</View>
 		</View>
 	)
@@ -54,12 +72,22 @@ const CaptureSection = ({ navigation, route }) => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: "#1e2833",
+		padding: 16
 	},
 	main: {
 		flex: 1,
 		flexDirection: "column",
-		backgroundColor: "#1e2833",
-		padding: 16,
+		justifyContent: "center",
+		gap: 32
+	},
+	form: {
+		gap: 16
+	},
+	controls: {
+		flexDirection: "row",
+		flexWrap: "wrap",
+		justifyContent: "center",
 		gap: 16
 	}
 });
