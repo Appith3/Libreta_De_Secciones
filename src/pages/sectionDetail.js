148465efe6c7fb78896ba5aa9ff363fef66ded16
appithe <joelaviles413@gmail.com>
2023-12-22 17:09:46 -0600

    Finish design Section Detail

--- src/pages/sectionDetail.js
+++ src/pages/sectionDetail.js
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
-import { StyleSheet, View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { useEffect, useState } from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import { Button, IconButton, Text } from "react-native-paper";
 
 const SectionDetail = (props) => {
 
@@ -17,19 +17,90 @@ const SectionDetail = (props) => {
 		codigo
 	} = route.params.cadenamiento
 
+	const [isEditable, setIsEditable] = useState(false)
+	const [isEditing, setIsEditing] = useState(false)
+
 	useEffect(() => {
 		navigation.setOptions({ title: `${nombre} ${codigo}` })
 	}, [])
 
+	const editText = () => {
+		setIsEditing(true)
+	}
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.main}>
-				<Text style={{ color: "#F5F7FA" }}>Elevación Central: {lectura_central}</Text>
+				<Text variant="headlineSmall" style={styles.title}>Elevación Central: {lectura_central}</Text>
 				<View style={styles.table}>
-					<View style={styles.col}>
-						<View style={styles.row}>
-
+					<View style={styles.row}>
+						<View style={[styles.col, styles.tableHeader, { borderTopLeftRadius: 8 }]}>
+							<Text variant="titleSmall" style={styles.tableHeaderText}>Distancia</Text>
 						</View>
+						<View style={[styles.col, styles.tableHeader]}>
+							<Text variant="titleSmall" style={styles.tableHeaderText}>Desnivel</Text>
+						</View>
+						<View style={[styles.col, styles.tableHeader]}>
+							{/* <View style={[styles.col, styles.tableHeader, { borderTopEndRadius: 8 }]}> */}
+							<Text variant="titleSmall" style={styles.tableHeaderText}>Altura</Text>
+						</View>
+						<View style={[styles.col, styles.tableHeader, { borderTopEndRadius: 8 }]}>
+							<Text variant="titleSmall" style={styles.tableHeaderText}>Editar</Text>
+						</View>
+					</View>
+					<View style={styles.row} id="1">
+						<TextInput
+							value="-20"
+							keyboardType="decimal-pad"
+							readOnly={!isEditable}
+							style={styles.tableRow}
+							// onEndEditing={() => setIsEditable(false)}
+						/>
+						<TextInput
+							value="-0.38"
+							keyboardType="decimal-pad"
+							style={styles.tableRow}
+						/>
+						<TextInput
+							value="2.79"
+							keyboardType="decimal-pad"
+							readOnly
+							style={styles.tableRow}
+						/>
+						<IconButton
+							icon="pencil"
+							mode="outlined"
+							style={styles.editButton}
+							iconColor="#B8E2C7"
+							onPress={editText}
+						/>
+					</View>
+					<View style={styles.row} id="2">
+						<TextInput
+							value="-20"
+							keyboardType="decimal-pad"
+							readOnly={!isEditable}
+							style={styles.tableRow}
+						/>
+						<TextInput
+							value="-0.38"
+							keyboardType="decimal-pad"
+							readOnly
+							style={styles.tableRow}
+						/>
+						<TextInput
+							value="2.79"
+							keyboardType="decimal-pad"
+							readOnly={!isEditable}
+							style={styles.tableRow}
+						/>
+						<IconButton
+							icon="pencil"
+							mode="outlined"
+							style={styles.editButton}
+							iconColor="#B8E2C7"
+							onPress={editText}
+						/>
 					</View>
 				</View>
 			</View>
@@ -46,17 +117,50 @@ const styles = StyleSheet.create({
 	main: {
 		flex: 1,
 		flexDirection: "column",
-		justifyContent: "center",
+		justifyContent: "flex-start",
 		gap: 32,
 	},
 	table: {
-		backgroundColor: "#A8BED1",
-		height: "90%"
+		borderColor: "#A8BED1",
+		borderRadius: 8,
+		borderWidth: 2
 	},
 	tableHeader: {
-		
+		backgroundColor: "#5D84A6",
+		// width: "33.33%",
+		width: "25%",
+	},
+	tableHeaderText: {
+		color: "#F5F7FA",
+		alignSelf: "center",
+		paddingHorizontal: 6,
+		paddingVertical: 8
+	},
+	row: {
+		flexDirection: "row",
+		justifyContent: "space-between"
+	},
+	tableRow: {
+		borderColor: "#A8BED1",
+		borderWidth: 1,
+		width: "25%",
+		// width: "33.33%",
+		textAlign: "center",
+		color: "#F5F7FA",
+		fontSize: 16,
+		paddingVertical: 8
+	},
+	editButton:{
+		width: "25%",
+		borderColor: "#A8BED1",
+		borderWidth: 1,
+		borderRadius: 0,
+		margin: 0,
+		height: "100%"
+	},
+	title: {
+		color: "#F5F7FA"
 	}
-
 });
 
 export default SectionDetail
