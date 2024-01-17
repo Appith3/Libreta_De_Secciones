f2a3382e5a8cd5aeb9aebcdc68f586f149c2e22a
joel aviles <joelaviles413@gmail.com>
2024-01-16 22:35:55 -0600

    refactor: delete unused imports

--- src/pages/captureSectionSides.js
+++ src/pages/captureSectionSides.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
-import { Button, Text, TextInput } from "react-native-paper";
+import { Button, TextInput } from "react-native-paper";
 
 const CaptureSectionSides = ({ navigation, route }) => {
 
